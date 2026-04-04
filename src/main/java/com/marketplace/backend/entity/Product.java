6185ced65d3fe371e7370b01d7225fe898468c77
Peak <ashira.c@ku.th>
2026-04-05 02:43:13 +0700

    feat: add Product entity

---
+++ src/main/java/com/marketplace/backend/entity/Product.java
@@ -0,0 +1,26 @@
+package com.marketplace.backend.entity;
+
+import jakarta.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private BigDecimal price;
+    private int stock;
+
+    public Long  getId() { return id; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public BigDecimal getPrice() { return price; }
+    public void setPrice(BigDecimal price) { this.price = price; }
+
+    public int getStock() { return stock; }
+    public void setStock(int stock) { this.stock = stock; }
+}
