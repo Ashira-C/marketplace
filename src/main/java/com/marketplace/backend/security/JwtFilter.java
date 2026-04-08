0173217b0d19b754523edcfb329766532ca0826b
Peak <ashira.c@ku.th>
2026-04-09 04:47:28 +0700

    add JwtFilter

---
+++ src/main/java/com/marketplace/backend/security/JwtFilter.java
@@ -0,0 +1,28 @@
+package com.marketplace.backend.security;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtFilter  extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String header = request.getHeader("Authorization");
+
+        if (header == null || !header.startsWith("Bearer ")) {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.getWriter().write("Invalid Authorization Token");
+            return;
+        }
+
+        String token = header.substring(7);
+
+        filterChain.doFilter(request, response);
+    }
+}
